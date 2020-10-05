4593ded6d0d50272a347d35ecc60b83b859e5c7a
luftkluft <luftkluft@gmail.com>
2020-10-05 19:02:49 +0300

    Dev: response '/' (airlines.json)

--- app/javascript/src/components/Airlines/Airlines.js
+++ app/javascript/src/components/Airlines/Airlines.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/v1/airlines.json')
+    .then(resp => console.log(resp))
+    .catch(resp => console.log(resp))
+  }, [airlines.length])
   return <div>Airlines#index view for app.</div>
 }
 
