53b800eb1d69a371885af6e1f75b635d62097d43
luftkluft <luftkluft@gmail.com>
2020-10-05 18:45:31 +0300

    Dev: react-router-dom: route, swich

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component, Fragment } from 'react'
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import Airlines from './Airlines/Airlines'
+import Airline from './Airline/Airline'
 
 class App extends Component {
-  render(){
-    return(
-      <div>Hello, World!</div>
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Airlines} />
+        <Route exact path="/airlines/:slug" component={Airline} />
+      </Switch>
     )
   }
 }
