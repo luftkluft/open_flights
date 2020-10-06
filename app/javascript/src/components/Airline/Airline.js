db371d5373b54661130335026f737023d115f98b
luftkluft <luftkluft@gmail.com>
2020-10-06 20:21:55 +0300

    Dev: view props data of show in consol.log

--- app/javascript/src/components/Airline/Airline.js
+++ app/javascript/src/components/Airline/Airline.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Airline = () => {
+const Airline = (props) => {
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+
+  useEffect(() => {
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+    axios.get(url)
+    .then(resp => console.log(resp))
+    .catch(resp => console.log(resp))
+  }, [])
   return <div>Airlines#show view for app.</div>
 }
 
