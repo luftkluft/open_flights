4815108dce614338b4f28a857005218466759726
luftkluft <luftkluft@gmail.com>
2020-10-06 21:15:48 +0300

    Dev: Airline loaded status

--- app/javascript/src/components/Airline/Airline.js
+++ app/javascript/src/components/Airline/Airline.js
@@ -6,17 +6,24 @@ import ReviewForm from './ReviewForm'
 const Airline = (props) => {
   const [airline, setAirline] = useState({})
   const [review, setReview] = useState({})
+  const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
     const slug = props.match.params.slug
     const url = `/api/v1/airlines/${slug}`
     axios.get(url)
-      .then(resp => setAirline(resp.data))
+      .then(resp => {
+        setAirline(resp.data)
+        setLoaded(true)
+      })
       .catch(resp => console.log(resp))
   }, [])
   return (
     <div>
-      <Header />
+      {
+        {loaded} &&
+        <Header />
+      }
       <ReviewForm />
     </div>
   )
