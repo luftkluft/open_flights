0d2aa5439cf098317010b2225c80c8e7b15e4517
luftkluft <luftkluft@gmail.com>
2020-10-08 23:57:41 +0300

    Dev: view reviews

--- app/javascript/src/components/Airline/Review.js
+++ app/javascript/src/components/Airline/Review.js
@@ -1,25 +1,84 @@
 import React from 'react'
+import styled from 'styled-components'
+import Rating from '../Rating/Rating'
+
+const Card = styled.div`
+  border-radius: 4px;
+  border: 1px solid #E6E6E6;
+  padding: 20px;
+  margin: 0px 0px 20px 0px;
+  position: relative;
+  margin-right: 12px;
+`
+
+const Title = styled.div`
+  padding: 20px 0px 0px 0px;
+  font-family: 'Poppins-Bold';
+  font-size: 18px;
+`
+
+const Description = styled.div`
+  padding: 0 0 20px 0;
+  font-size: 14px;
+`
+const Options = styled.div`
+position:absolute;
+right :15px;
+top: 15px;
+display: flex;
+flex-direction: columns;
+`
+
+const Icon = styled.button`
+  box-shadow: none;
+  border-radius: 4px;
+  margin: 0 4px;
+
+  i {
+    font-size: 18px;
+  }
+`
+
+const Author = styled.div`
+  font-size: 16px;
+  font-family: 'Poppins-Bold';
+  margin: 0 8px;
+`
+
+const RatingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+const AvatarWrapper = styled.div`
+  width: 25px;
+  height: 25px;
+  background: green;
+  border-radius: 100%;
+  margin-right: 12px;
+  margin-bottom: -12px;
+
+  svg {
+    width: 25px;
+    height: 25px;
+  }
+`
 
 const Review = (props) => {
-  console.log('%%%%%%%%%%%%%%%%%%%')
-  const score = props.attributes.score
-  const title = props.attributes.title
-  const description = props.attributes.description
-  console.log(props.attributes)
+  const attributes = props.attributes
 
   return (
     <div>
-      <div>
-        <div>
-          {score}
-        </div>
-        <div>
-          {title}
-        </div>
-        <div>
-          {description}
-        </div>
-      </div>
+      <Card>
+        <RatingContainer>
+        <Rating score={attributes.score} />
+        </RatingContainer>
+        <Title>
+          {attributes.title}
+        </Title>
+        <Description>
+          {attributes.description}
+        </Description>
+      </Card>
     </div>
   )
 }
